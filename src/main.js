5fd1e5dd5a1a9765e71f3ce124416da3373f9cb1
Zach Edwards <zachary@zachs-mbp.lan>
2021-11-18 15:18:58 -0500

    init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import BlokInk from 'storyblok-design-system'
-import 'storyblok-design-system/dist/storyblok-design-system.css'
+// import BlokInk from 'storyblok-design-system'
+// import 'storyblok-design-system/dist/storyblok-design-system.css'
 
-createApp(App).use(store).use(BlokInk).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
